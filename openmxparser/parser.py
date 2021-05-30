28ed8bfbafb6218fd8f706dff17ac164f3c83fb2
Md Morshadur Rahman <65698541+Mdmorshadurrahman@users.noreply.github.com>
2021-05-30 13:23:58 +0200

    Detect also LDA functionals
    Co-authored-by: Pavel Ondračka <pavel.ondracka@gmail.com>

--- openmxparser/parser.py
+++ openmxparser/parser.py
@@ -134,14 +134,17 @@ class OpenmxParser(FairdiParser):
             if mainfile_parser.get('scf_hubbard_u').lower == 'on':
                 method.electronic_structure_method = 'DFT+U'
 
-        section_XC_functionals1 = method.m_create(xc_functionals)
-        section_XC_functionals2 = method.m_create(xc_functionals)
-
+        xc_functional_dictionary = {
+            'GGA-PBE': ['GGA_C_PBE', 'GGA_X_PBE'],
+            'LDA': ['LDA_X', 'LDA_C_PZ'],
+            'LSDA-CA': ['LDA_X', 'LDA_C_PZ'],
+            'LSDA-PW': ['LDA_X', 'LDA_C_PW'],
+            None: ['LDA_X', 'LDA_C_PZ']
+        }
         scf_XcType = mainfile_parser.get('scf_XcType')
-        if scf_XcType is not None:
-            if mainfile_parser.get('scf_XcType').upper() == 'GGA-PBE':
-                section_XC_functionals1.XC_functional_name = 'GGA_C_PBE'
-                section_XC_functionals2.XC_functional_name = 'GGA_X_PBE'
+        for xc in xc_functional_dictionary[scf_XcType]:
+            method.m_create(xc_functionals).XC_functional_name = xc
+        scf_XcType = mainfile_parser.get('scf_XcType')
 
         scf_SpinPolarizationType = mainfile_parser.get('scf_SpinPolarization')
         if scf_SpinPolarizationType.lower() == 'on':
